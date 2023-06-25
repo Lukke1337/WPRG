fafad07b103f934e398c5eb80782160e717a9a64
Lukke1337 <119882873+Lukke1337@users.noreply.github.com>
2023-06-25 21:37:00 +0200

    Delete require_all.php

--- require_all.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Pobranie wszystkich nazw ścieżek z folderu "klasy" i robi "require_once" dla każdego z tych plików
-    $files = glob( 'klasy/*.php');
-    foreach ($files as $file) {
-    require_once($file);
-}
